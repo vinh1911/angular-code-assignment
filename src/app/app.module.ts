a86167fede0651baa068cb30fca43a1a9dc772a1
captainmayo <reventon1911@gmail.com>
2023-04-11 15:59:29 +0700

    add ngrx and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {BackendService} from './backend.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [BackendService],
   bootstrap: [AppComponent]
